e86efb193f912bd863b31e9ff95c49bf30cbb99f
limon <montoyalinaj@gmail.com>
2020-04-16 18:42:42 +0200

    Intro-lesson-5
    Event Handling

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ var app = new Vue({
 		inStock: true,
 		details: ["80% cotton", "20% polyester", "Gender-neutral"],
 		variants: [
-			{variantId: 2234, variantColor: "green"},
-			{variantId: 2235, variantColor: "blue" }
+			{variantId: 2234, variantColor: "green", variantImage: 'assets/vmSocks-green-onWhite.jpg'},
+			{variantId: 2235, variantColor: "blue" , variantImage: 'assets/vmSocks-blue-onWhite.jpg' }
 		],
-		sizes: ["Small", "Medium", "Large", "Extralarge"]
+		cart1: 0,
+		cart: 0
+	},
+	methods:{
+		addToCart() {
+			this.cart ++
+		},
+		updateProduct: function (variantImage){
+			this.myimage = variantImage
+		},
+		removeValCart(){
+			this.cart --
+		}
 	}
 });
